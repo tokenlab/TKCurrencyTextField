5938e4010626e1de55aea8ce7debf008521de406
Daniele Boscolo <danielehidalgo@gmail.com>
2017-06-06 14:13:02 -0300

    Implement mixin protocol

--- TKCurrencyTextField/Classes/TKCurrencyTextField.swift
+++ TKCurrencyTextField/Classes/TKCurrencyTextField.swift
@@ -8,12 +8,107 @@
 
 import UIKit
 
+public protocol TKCurrencyTextFieldMixin {
+    var amount: Double {get set}
+    var maxDigits: Int {get set}
+    var defaultValue: Double {get set}
+    var currencySymbol: Bool {get set}
+    var locale: Locale {get set}
+    
+    func textDidChange()
+    func initTextField()
+    func setAmount(_ amount: Double)
+    func getAmount() -> Double
+    func cannotPerformPastCopyAndCutAction(_ action: Selector, withSender sender: Any?) -> Bool
+}
+
+public extension TKCurrencyTextFieldMixin where Self: UITextField {
+    
+    func getAmount() -> Double {
+        if let text = text {
+            return text.currencyStringToDouble(with: locale)
+        }
+        return defaultValue
+    }
+    
+    func setAmount(_ amount: Double) {
+        let textFieldStringValue = amount.currencyStringValue(with: locale, currencySymbol)
+        text = textFieldStringValue
+        textDidChange()
+    }
+    
+    func textDidChange() {
+        let maskHandler: ReplacementStringHandler = {
+            (textField, range, replacementString) in
+            let nsString: NSString = textField.text as NSString? ?? ""
+            let text = nsString.replacingCharacters(in: range, with: replacementString)
+            
+            //Remove mask from newText
+            var cleanNumericString: String = text.onlyNumberString
+            var textFieldNumber: Double = 0.0
+            
+            //Check if currency symbol was replaced
+            // In case currency symbol was replaced, erase last character
+            let formatter:NumberFormatter = NumberFormatter()
+            formatter.locale = self.locale
+            formatter.numberStyle = .currency
+            if self.currencySymbol && !text.contains(formatter.currencySymbol) && cleanNumericString.characters.count > 0 {
+                let lastIndex: String.Index = cleanNumericString.endIndex;
+                cleanNumericString = cleanNumericString.substring(to: cleanNumericString.index(before: lastIndex))
+            }
+            
+            //Checks if number of digits exceeded max
+            if cleanNumericString.characters.count > self.maxDigits {
+                let limitString = cleanNumericString.substring(to: cleanNumericString.characters.index(cleanNumericString.startIndex, offsetBy: self.maxDigits))
+                textFieldNumber = (limitString as NSString).doubleValue
+            } else {
+                textFieldNumber = (cleanNumericString as NSString).doubleValue
+            }
+            
+            //Applies mask
+            let textFieldNewValue = textFieldNumber/100
+            let textFieldStringValue = textFieldNewValue.currencyStringValue(with: self.locale, self.currencySymbol)
+            self.text = textFieldStringValue
+        }
+        maskHandler(self, NSRange(), "")
+    }
+    
+    func initTextField() {
+        keyboardType = .decimalPad
+        setAmount(defaultValue)
+    }
+    
+    func closestPosition(to point: CGPoint) -> UITextPosition? {
+        let beginning:UITextPosition = self.beginningOfDocument
+        let end:UITextPosition? = self.position(from: beginning, offset: self.text?.characters.count ?? 0)
+        return end
+    }
+    
+    func cannotPerformPastCopyAndCutAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        if action == #selector(UITextField.paste(_:))
+            || action == #selector(UITextField.copy(_:))
+            || action == #selector(UITextField.cut(_:)) {
+            return false
+        }
+        return true
+    }
+}
+
 public typealias ReplacementString = String
 public typealias ReplacementStringHandler = (UITextField, NSRange, ReplacementString) -> () // Can be used on textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool
 
-@IBDesignable open class TKCurrencyTextField: UITextField {
+@IBDesignable open class TKCurrencyTextField: UITextField, TKCurrencyTextFieldMixin {
     
     // MARK:- Public
+    
+    public var amount: Double {
+        get {
+            return getAmount()
+        }
+        set {
+            setAmount(newValue)
+        }
+    }
     
     @IBInspectable public var maxDigits: Int = 15
     @IBInspectable public var defaultValue: Double = 0.00 {
@@ -21,63 +116,17 @@ public typealias ReplacementStringHandler = (UITextField, NSRange, ReplacementSt
             self.textDidChange()
         }
     }
+    
     @IBInspectable public var currencySymbol: Bool = true {
         didSet {
             self.textDidChange()
         }
     }
+    
     public var locale: Locale = Locale.current {
         didSet {
             self.textDidChange()
         }
-    }
-    
-    public var amount: Double {
-        get {
-            if let text = text {
-                return text.currencyStringToDouble(with: locale)
-            }
-            
-            return defaultValue
-        }
-        
-        set {
-            let textFieldStringValue = amount.currencyStringValue(with: locale, currencySymbol)
-            text = textFieldStringValue
-        }
-    }
-
-    lazy var maskHandler: ReplacementStringHandler = {
-        (textField, range, replacementString) in
-        let nsString: NSString = textField.text as NSString? ?? ""
-        let text = nsString.replacingCharacters(in: range, with: replacementString)
-        
-        //Remove mask from newText
-        var cleanNumericString: String = text.onlyNumberString
-        var textFieldNumber: Double = 0.0
-        
-        //Check if currency symbol was replaced
-        // In case currency symbol was replaced, erase last character
-        let formatter:NumberFormatter = NumberFormatter()
-        formatter.locale = self.locale
-        formatter.numberStyle = .currency
-        if self.currencySymbol && !text.contains(formatter.currencySymbol) && cleanNumericString.characters.count > 0 {
-            let lastIndex: String.Index = cleanNumericString.endIndex;
-            cleanNumericString = cleanNumericString.substring(to: cleanNumericString.index(before: lastIndex))
-        }
-        
-        //Checks if number of digits exceeded max
-        if cleanNumericString.characters.count > self.maxDigits {
-            let limitString = cleanNumericString.substring(to: cleanNumericString.characters.index(cleanNumericString.startIndex, offsetBy: self.maxDigits))
-            textFieldNumber = (limitString as NSString).doubleValue
-        } else {
-            textFieldNumber = (cleanNumericString as NSString).doubleValue
-        }
-        
-        //Applies mask
-        let textFieldNewValue = textFieldNumber/100
-        let textFieldStringValue = textFieldNewValue.currencyStringValue(with: self.locale, self.currencySymbol)
-        textField.text = textFieldStringValue
     }
     
     // MARK: - init functions
@@ -85,44 +134,30 @@ public typealias ReplacementStringHandler = (UITextField, NSRange, ReplacementSt
     override public init(frame: CGRect) {
         super.init(frame: frame)
         initTextField()
+        addTarget()
     }
     
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         initTextField()
+        addTarget()
     }
     
     open override func awakeFromNib() {
         super.awakeFromNib()
         initTextField()
+        addTarget()
     }
     
-    private func initTextField(){
-        amount = defaultValue
-        keyboardType = .decimalPad
-        addTarget(self, action: #selector(textDidChange), for: .editingChanged)
-
+    func addTarget() {
+        addTarget(self, action: #selector(editingChanged(_:)), for: .editingChanged)
     }
     
-    func textDidChange() {
-        // get the original position of the cursor
-        maskHandler(self, NSRange(), "")
+    @objc func editingChanged(_ sender: Any?) {
+        textDidChange()
     }
     
-    override open func closestPosition(to point: CGPoint) -> UITextPosition? {
-        let beginning:UITextPosition = self.beginningOfDocument
-        let end:UITextPosition? = self.position(from: beginning, offset: self.text?.characters.count ?? 0)
-        return end
-    }
-    
-    open override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
-        
-        if action == #selector(UITextField.paste(_:))
-        || action == #selector(UITextField.copy(_:))
-        || action == #selector(UITextField.cut(_:)) {
-            return false
-        }
-        
-        return super.canPerformAction(action, withSender: sender)
+    override open func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        return cannotPerformPastCopyAndCutAction(action, withSender: sender)
     }
 }
--- TKCurrencyTextFieldTests/TKCurrencyTextFieldTests.swift
+++ TKCurrencyTextFieldTests/TKCurrencyTextFieldTests.swift
@@ -10,72 +10,75 @@ import XCTest
 @testable import TKCurrencyTextField
 
 class TKCurrencyTextFieldTests: XCTestCase {
-    
+
+    var currencyTextField: TKCurrencyTextField!
+
     override func setUp() {
         super.setUp()
+        currencyTextField = TKCurrencyTextField()
     }
     
     override func tearDown() {
         super.tearDown()
+        currencyTextField = nil
+    }
+    
+    func testInitialValue() {
+        currencyTextField.locale = Locale(identifier: "en_US")
+        print("currencyTextField.text = \(currencyTextField.text ?? "")")
+        XCTAssert(currencyTextField.text == "$0.00")
     }
     
     func testLocale_en_US() {
-        let currencyTextField = TKCurrencyTextField()
         currencyTextField.locale = Locale(identifier: "en_US")
-        currencyTextField.setAmount(1000.49)
+        currencyTextField.amount = 1000.49
+        print("currencyTextField.text = \(currencyTextField.text ?? "")")
         XCTAssert(currencyTextField.text == "$1,000.49")
     }
     
     func testLocale_en_US_noCurrencySymbol() {
-        let currencyTextField = TKCurrencyTextField()
         currencyTextField.locale = Locale(identifier: "pt_BR")
         currencyTextField.currencySymbol = false
-        currencyTextField.setAmount(1000.49)
+        currencyTextField.amount = 1000.49
         XCTAssert(currencyTextField.text == "1.000,49")
     }
     
     func testLocale_pt_BR() {
-        let currencyTextField = TKCurrencyTextField()
         currencyTextField.locale = Locale(identifier: "pt_BR")
-        currencyTextField.setAmount(1000.49)
+        currencyTextField.amount = 1000.49
         XCTAssert(currencyTextField.text == "R$1.000,49")
     }
     
     func testLocale_pt_BR_noCurrencySymbol() {
-        let currencyTextField = TKCurrencyTextField()
         currencyTextField.locale = Locale(identifier: "pt_BR")
         currencyTextField.currencySymbol = false
-        currencyTextField.setAmount(1000.49)
+        currencyTextField.amount = 1000.49
         XCTAssert(currencyTextField.text == "1.000,49")
     }
     
     func testLocale_en_GB() {
-        let currencyTextField = TKCurrencyTextField()
         currencyTextField.locale = Locale(identifier: "en_GB")
-        currencyTextField.setAmount(2199999.21)
+        currencyTextField.amount = 2199999.21
         XCTAssert(currencyTextField.text == "£2,199,999.21")
     }
     
     func testLocale_en_GB_noCurrencySymbol() {
-        let currencyTextField = TKCurrencyTextField()
         currencyTextField.locale = Locale(identifier: "en_GB")
         currencyTextField.currencySymbol = false
-        currencyTextField.setAmount(2199999.22)
+        currencyTextField.amount = 2199999.22
         XCTAssert(currencyTextField.text == "2,199,999.22")
     }
     
     func testLocale_es_ES() {
-        let currencyTextField = TKCurrencyTextField()
         currencyTextField.locale = Locale(identifier: "es_ES")
-        currencyTextField.setAmount(1234.56)
+        currencyTextField.amount = 1234.56
         XCTAssert(currencyTextField.text == "1.234,56 €")
     }
     
     func testLocale_es_ES_noCurrencySymbol() {
-        let currencyTextField = TKCurrencyTextField()
         currencyTextField.locale = Locale(identifier: "es_ES")
         currencyTextField.currencySymbol = false
-        currencyTextField.setAmount(1234.56)
+        currencyTextField.amount = 1234.56
         XCTAssert(currencyTextField.text == "1.234,56 ")
     }
 }
